cf4435d3bd6ab5920811b71e8fe7abf1859cfe93
Diego Valles <dgvall134@gmail.com>
2022-09-06 20:59:44 -0600

    commit tree 183ada2a8f77f11d1f1709158e5afdbf4381b579

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(array, string) {
+    const nameArr = array.filter(element => element.toLowerCase() === string.toLowerCase())
+    return nameArr
+}
+
+function fuzzyMatch(array, string) {
+    const nameArr = array.filter(element => element.charAt(0) === string.charAt(0))
+    return nameArr
+}
+
+function matchName(array, string) {
+    const nameArr = array.filter(element => element.name === string)
+    return nameArr
+}
